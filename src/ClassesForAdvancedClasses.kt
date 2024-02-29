b96c75dd895eba03941f44ac657d5cfc8c98e177
enokdragon <111323615+enokdragon@users.noreply.github.com>
2024-02-29 13:07:29 -0600

    Update ClassesForAdvancedClasses.kt
    not broken any longer

--- src/ClassesForAdvancedClasses.kt
+++ src/ClassesForAdvancedClasses.kt
@@ -1,29 +1,29 @@
 class Employee(var empName: String,  //name (string) , position (string) , salary (boolean), payRate (double) shift (int)
-               var position: String,
+               var position: PositionTitle,
                var salary: Boolean,
                var payRate: Double,
                var shift: Int) {
 
     fun display() {
         println(empName.toString() + "\n" +
-                "Employee Position:"+ PositionTitle(position)+ "\n" +
+                "Employee Position:"+ position.name + "\n" +
                 "Salary:" + salary + "\n" +
                 "Payrate:" + payRate + "\n" +
                 "Shift:" + shift)
     }
 
     fun calculate(hoursWorked: Double){
-        var newPay = Double
+        var newPay: Double = 0.00
         var rate = payRate
         var unpaidHours = hoursWorked
-        var overtime = Double
+        var overtime: Double = 0.00
         if(salary == false) {
             while (unpaidHours > 40) {
-                overtime = overtime + rate * 1.5
+                overtime = overtime + rate * 1.50
                 unpaidHours--
             }
         }
-        var paycheck = Double
+        var paycheck: Double = 0.00
         paycheck = overtime + rate * unpaidHours
 
         if(shift == 2){
@@ -35,23 +35,25 @@ class Employee(var empName: String,  //name (string) , position (string) , salar
         println("$empName made $$paycheck this week at their position in " +
                 "$position at a rate of $$payRate per hour.")
     }
-}
-enum class PositionTitle{
-    ADMINISTRATION,
-    PRODUCTION,
-    SALES,
-    MAINTENANCE,
-    TECHNICAL,
-    SECRETARIAL;
-}
 
-//fun PrintPosition(pos: PositionTitle): String{
-  //  when(pos){
-    //    PositionTitle.ADMINISTRATION -> return "Administration"
-      //  PositionTitle.PRODUCTION -> return "Production"
-        //PositionTitle.SALES -> return "Sales"
-   //     PositionTitle.MAINTENANCE -> return "Maintenance"
-     //   PositionTitle.TECHNICAL -> return "Technical"
-       // PositionTitle.SECRETARIAL -> return "Secretarial"
-   // }
-//}
+    fun PrintPosition(pos: PositionTitle): String{
+        when(pos){
+            PositionTitle.ADMINISTRATION -> return "Administration"
+            PositionTitle.PRODUCTION -> return "Production"
+            PositionTitle.SALES -> return "Sales"
+            PositionTitle.MAINTENANCE -> return "Maintenance"
+            PositionTitle.TECHNICAL -> return "Technical"
+            PositionTitle.SECRETARIAL -> return "Secretarial"
+        }
+    }
+
+//    enum class PositionTitle(name: String) {
+//        ADMINISTRATION("a"),
+//        PRODUCTION("b"),
+//        SALES("c"),
+//        MAINTENANCE("d"),
+//        TECHNICAL("e"),
+//        SECRETARIAL("f");
+//    }
+
+}
