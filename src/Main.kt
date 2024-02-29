2230147bf03eb155acdd11d0721c2c2bd2268465
enokdragon <111323615+enokdragon@users.noreply.github.com>
2024-02-29 13:06:39 -0600

    Update Main.kt
    working now

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,17 @@
-public static void main(String[] args) {
+public fun main() {
     var emp1 = Employee("Jane Doe", PositionTitle.ADMINISTRATION, true, 27.00, 1);
     var emp2 = Employee("Larry Jackson", PositionTitle.PRODUCTION, false, 22.00, 2);
     var emp3 = Employee("John Smith", PositionTitle.MAINTENANCE, false, 21.00, 3);
 
 
     emp1.display();
-    emp1.calculate(45);
+    emp1.calculate(45.0);
     emp2.display();
-    emp2.calculate(50);
+    emp2.calculate(50.0);
     emp3.display();
-    emp3.calculate(35);
+    emp3.calculate(35.0);
 }
+
+
+
 
